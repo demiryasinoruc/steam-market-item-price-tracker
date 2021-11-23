bcb7edc4ea757f9733fcd5cbe4cba1ec18d1d8de
Demir Yasin ORUÇ <demiryasinoruc@gmail.com>
2021-11-24 02:32:54 +0300

    feat: changelog/ twitter url's, popup design

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,5 +3,8 @@ module.exports = {
     type: 'basic',
     iconUrl: 'assets/icons/icon_128.png'
   },
-  INSTALLATION_URL: 'https://steamcommunity.com/market?smipt=installation'
+  INSTALLATION_URL: 'https://steamcommunity.com/market?smipt=installation',
+  TWITTER_URL: 'https://twitter.com/steamextensions',
+  CHANGELOG_URL:
+    'https://github.com/demiryasinoruc/steam-market-item-price-tracker/blob/master/CHANGELOG.md'
 }
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,106 +1,140 @@
 <template>
-  <div class="container-fluid p-3 border">
-    <div class="row ">
-      <div class="col">
-        <h5 class="mb-3">
-          {{ translations.trackingList }} ({{ trackings.length }})
-          <button
-            class="btn btn-outline-info btn-sm float-right ml-2"
-            :title="translations.openSettings"
-            @click="openPage('options.html')"
-          >
-            <i class="fa fa-cog"></i>
-          </button>
-          <button
-            class="btn btn-outline-primary btn-sm float-right ml-2"
-            :title="translations.openTrackingList"
-            @click="openPage('list.html')"
-          >
-            <i class="fa fa-list"></i>
-          </button>
-          <button
-            v-show="status"
-            class="btn btn-outline-success btn-sm float-right ml-2"
-            :title="translations.pause"
-            @click="changeStatus(false)"
-          >
-            <i class="fa fa-pause"></i>
-          </button>
-          <button
-            v-show="!status"
-            class="btn btn-outline-warning btn-sm float-right"
-            :title="translations.continue"
-            @click="changeStatus(true)"
-          >
-            <i class="fa fa-play"></i>
-          </button>
-        </h5>
-        <ul
-          v-if="trackings.length > 0"
-          class="list-group mt-3"
-        >
-          <li
-            v-for="tracking in trackingList"
-            :key="tracking.appid"
-            class="list-group-item"
-          >
-            <span>
-              {{ tracking.appname }}
-            </span>
-            <span class="badge badge-danger float-right mt-1">
-              {{ itemCountsMessage(tracking.count) }}
-            </span>
-          </li>
-        </ul>
-        <div
-          v-else
-          class="alert alert-info"
-        >
-          <i class="fa fa-info-circle"></i>
-          {{ translations.trackingListEmpty }}
+  <div class="container-fluid p-0">
+    <div class="card">
+      <div class="card-body">
+        <div class="row ">
+          <div class="col">
+            <h5 class="mb-3">
+              {{ translations.trackingList }} ({{ trackings.length }})
+            </h5>
+            <ul
+              v-if="trackings.length > 0"
+              class="list-group mt-3"
+            >
+              <li
+                v-for="tracking in trackingList"
+                :key="tracking.appid"
+                class="list-group-item"
+              >
+                <span>
+                  {{ tracking.appname }}
+                </span>
+                <span class="badge badge-danger float-right mt-1">
+                  {{ itemCountsMessage(tracking.count) }}
+                </span>
+              </li>
+            </ul>
+            <div
+              v-else
+              class="alert alert-info"
+            >
+              <i class="fa fa-info-circle"></i>
+              {{ translations.trackingListEmpty }}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-12">
-        <h5 class="mt-3">
-          {{ translations.notifications }} ({{ notifications.length }})
-        </h5>
-        <!-- create bootstrap unstyled list -->
-        <div
-          v-for="(notification,index) in notificationList"
-          :key="index"
-          class="d-flex justify-content-end border mt-2"
-        >
-          <button
-            class="btn btn-link text-left flex-fill"
-            target="_blank"
-            :title="translations.openListingPage"
-            @click="openListingPage(notification)"
-          >
-            {{ notification.item.name }}
-          </button>
-          <button
-            class="btn btn-outline-warning m-1"
-            :title="translations.triggerNotification"
-            @click="triggerNotification(notification)"
-          >
-            <i class="fa fa-refresh"></i>
-          </button>
-          <button
-            class="btn btn-outline-danger m-1"
-            :title="translations.triggerNotification"
-            @click="removeNotification(notification)"
-          >
-            <i class="fa fa-times"></i>
-          </button>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-sm-12">
+            <h5>
+              {{ translations.notifications }} ({{ notifications.length }})
+            </h5>
+            <div
+              v-for="(notification,index) in notificationList"
+              :key="index"
+              class="d-flex justify-content-end border mt-2"
+            >
+              <button
+                class="btn btn-link text-left flex-fill"
+                target="_blank"
+                :title="translations.openListingPage"
+                @click="openListingPage(notification)"
+              >
+                {{ notification.item.name }}
+              </button>
+              <button
+                class="btn btn-outline-warning m-1"
+                :title="translations.triggerNotification"
+                @click="triggerNotification(notification)"
+              >
+                <i class="fa fa-refresh"></i>
+              </button>
+              <button
+                class="btn btn-outline-danger m-1"
+                :title="translations.triggerNotification"
+                @click="removeNotification(notification)"
+              >
+                <i class="fa fa-times"></i>
+              </button>
+            </div>
+            <div
+              v-show="notifications.length === 0"
+              class="alert alert-info"
+            >
+              <i class="fa fa-info-circle"></i>
+              {{ translations.noNewNotification }}
+            </div>
+          </div>
         </div>
-        <div
-          v-show="notifications.length === 0"
-          class="alert alert-info"
-        >
-          <i class="fa fa-info-circle"></i>
-          {{ translations.noNewNotification }}
+      </div>
+      <div class="card-footer">
+        <div class="row">
+          <div class="col-3">
+            <span class="version">
+              {{ translations.version }}: <strong> {{ version }}</strong>
+            </span>
+          </div>
+          <div class="col-9 text-right">
+
+            <a
+              :title="translations.goToTwitter"
+              class="btn btn-outline-info"
+              target="_blank"
+              :href="changeLog"
+            >
+              <i class="fa fa-history"></i>
+            </a>
+            <a
+              :title="translations.goToTwitter"
+              class="btn btn-outline-info"
+              target="_blank"
+              href="https://twitter.com/steamextensions"
+            >
+              <i class="fa fa-twitter"></i>
+            </a>
+            <span class="mx-2">|</span>
+            <button
+              v-show="status"
+              class="btn btn-outline-success"
+              :title="translations.pause"
+              @click="changeStatus(false)"
+            >
+              <i class="fa fa-pause"></i>
+            </button>
+            <button
+              v-show="!status"
+              class="btn btn-outline-warning"
+              :title="translations.continue"
+              @click="changeStatus(true)"
+            >
+              <i class="fa fa-play"></i>
+            </button>
+            <button
+              class="btn btn-outline-primary"
+              :title="translations.openTrackingList"
+              @click="openPage('list.html')"
+            >
+              <i class="fa fa-list"></i>
+            </button>
+            <button
+              class="btn btn-outline-info"
+              :title="translations.openSettings"
+              @click="openPage('options.html')"
+            >
+              <i class="fa fa-cog"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -110,7 +144,7 @@
 <script>
 import browser from 'webextension-polyfill'
 import TRANSLATIONS from '../_locales/en/strings.json'
-import { INSTALLATION_URL } from '../common/constants'
+import { INSTALLATION_URL, TWITTER_URL, CHANGELOG_URL } from '../common/constants'
 import KEYS from '../common/keys'
 
 export default {
@@ -120,7 +154,10 @@ export default {
       translations: TRANSLATIONS,
       notifications: [],
       trackings: [],
-      status: false
+      status: false,
+      twitter: TWITTER_URL,
+      changeLog: CHANGELOG_URL,
+      version: ''
     }
   },
   computed: {
@@ -135,6 +172,7 @@ export default {
     }
   },
   async created() {
+    this.version = browser.runtime.getManifest().version
     this.getTranslations()
     this.getData()
     const that = this
@@ -240,5 +278,9 @@ body {
 
 .list-group-item {
   padding: 0.5rem 0.75rem;
+}
+
+.version {
+  line-height: 38px;
 }
 </style>
