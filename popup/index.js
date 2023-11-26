cda550fd8c616c15609b6b7b19afeb7632363763
demiryasinoruc <demiryasinoruc@gmail.com>
2023-11-27 01:42:45 +0300

    test

--- popup/index.js
+++ popup/index.js
@@ -1,6 +1,190 @@
+const browser = chrome || browser
+let translations = {}
+let appStatus = false
+let notifications = []
+
+const trackListRowTemplate =
+          `
+            <li class="list-group-item">
+                <span>{{APPNAME}}</span>
+                <span class="badge badge-danger float-right mt-1">{{ITEMCOUNT}}</span>
+            </li>
+          `
+
+window.onload = async function () {
+
+  /// TODO: get translations
+  translations = await getTranslations()
+
+  /// TODO: get trackings
+  const trackings = await getTrackings()
+
+  /// TODO: set trackings
+  setTrackings(trackings)
+
+  /// TODO: get notifications
+  getNotifications()
+
+  /// TODO: set notifications
+  setNotifications()
+
+  /// TODO: get status
+
+  /// TODO: get version
+  /// TODO: set version
+
+  /// TODO: get settings
+  /// TODO: set settings
+
+
+
+
+  appStatus = await getStatus()
+
+
+
+
+
+  setVersion()
+}
+
+async function getTranslations() {
+  return await browser.runtime.sendMessage({
+    type: keys.GET_TRANSLATIONS
+  })
+}
+
+async function getTrackings() {
+  const { trackList } = await browser.storage.local.get({
+    trackList: []
+  })
+
+  const trackListGrouped = trackList.reduce((acc, item) => {
+    const { appname } = item
+
+    if (!acc[appname]) {
+      acc[appname] = []
+    }
+
+    acc[appname].push(item)
+
+    return acc
+  }, {})
+
+  return Object.keys(trackListGrouped).map((appname) => {
+    return { appname, count: trackListGrouped[appname].length }
+  })
+}
+
+function setTrackings(trackings) {
+  const pageHeader = document.querySelector('#page-header')
+  pageHeader.innerHTML = `${translations.trackingList} (${trackings.length})`
+
+  if (trackings.length === 0) {
+    const trackingListAlert = document.querySelector('#track-list-empty-alert')
+    trackingListAlert.querySelector('span').innerHTML =
+      translations.trackingListEmpty
+    trackingListAlert.classList.remove('d-none')
+    return
+  }
+
+  let trackListRows = ''
+
+  for (let i = 0; i < trackings.length; i++) {
+    const row = trackings[i]
+    trackListRows += trackListRowTemplate
+      .replace('{{APPNAME}}', row.appname)
+      .replace('{{ITEMCOUNT}}', itemCountsMessage(row.count))
+  }
+
+  const trackList = document.querySelector('#track-list')
+  trackList.innerHTML = trackListRows
+}
+
+async function getNotifications() {
+  notifications = await browser.runtime.sendMessage({
+    type: keys.GET_NOTIFICATIONS
+  })
+}
+
+function setNotifications() {
+  const noNotificationAlert = document.querySelector('#no-notification')
+
+  noNotificationAlert.innerHTML = `<i class="fa fa-info-circle"></i> ${translations.noNewNotification}`
+
+  if (notifications.length === 0) {
+    noNotificationAlert.classList.remove('d-none')
+  }
+}
+
+function setVersion() {
+  const versionHolder = document.querySelector('#version')
+  const version = browser.runtime.getManifest().version
+
+  versionHolder.innerHTML = `${translations.version}: <strong> ${version}</strong>`
+}
+
+async function openPage(url) {
+  await browser.tabs.create({ url })
+}
+
+async function openAll() {
+  if (openAllStatus) {
+    return
+  }
+
+  openAllStatus = true
+
+  await browser.runtime.sendMessage({
+    type: keys.OPEN_ALL_NOTIFICATIONS
+  })
+}
+
+async function getStatus() {
+  return await browser.runtime.sendMessage({
+    type: keys.GET_STATUS
+  })
+}
+
+async function changeStatus(status) {
+  await browser.runtime.sendMessage({
+    type: keys.SET_STATUS,
+    status
+  })
+  getStatus()
+}
+
+async function removeNotification(notification) {
+  notifications = notifications.filter(
+    (item) => item.name !== notification.name
+  )
+  await browser.runtime.sendMessage({
+    type: keys.REMOVE_NOTIFICATION,
+    notification
+  })
+}
+
+async function triggerNotification(notification) {
+  await browser.runtime.sendMessage({
+    type: keys.TRIGGER_NOTIFICATION,
+    notification
+  })
+}
+
+async function openListingPage(notification) {
+  await browser.runtime.sendMessage({
+    type: keys.OPEN_NOTIFICATION,
+    notification
+  })
+}
+
+function itemCountsMessage(itemCount) {
+  return translations.itemCounts.replace('{{ITEMCOUNT}}', itemCount)
+}
+
 //  import '../assets/scss/custom-bootstrap.scss'
 
-// const browser = chrome || browser
+//
 // const TRANSLATIONS = {}
 
 // new Vue({
@@ -41,89 +225,6 @@
 //     })
 //   },
 //   methods: {
-//     async getTranslations() {
-//       const { translations } = await browser.runtime.sendMessage({
-//         type: keys.GET_TRANSLATIONS
-//       })
-//       this.translations = translations
-//     },
-//     async openPage(url) {
-//       await browser.tabs.create({ url })
-//     },
-//     async openAll() {
-//       if (this.openAllStatus) {
-//         return
-//       }
-//       this.openAllStatus = true
-//       await browser.runtime.sendMessage({
-//         type: keys.OPEN_ALL_NOTIFICATIONS
-//       })
-//     },
-//     async getData() {
-//       this.getStatus()
-//       this.getTrackings()
-//       this.getNotifications()
-//     },
-//     async getTrackings() {
-//       const { trackList } = await browser.storage.local.get({
-//         trackList: []
-//       })
-//       const trackListGrouped = trackList.reduce((acc, item) => {
-//         const { appname } = item
-//         if (!acc[appname]) {
-//           acc[appname] = []
-//         }
-//         acc[appname].push(item)
-//         return acc
-//       }, {})
-//       const trackings = Object.keys(trackListGrouped).map((appname) => {
-//         return { appname, count: trackListGrouped[appname].length }
-//       })
-//       this.trackings = trackings
-//     },
-//     async getNotifications() {
-//       this.notifications = await browser.runtime.sendMessage({
-//         type: keys.GET_NOTIFICATIONS
-//       })
-//     },
-//     async getStatus() {
-//       this.status = await browser.runtime.sendMessage({
-//         type: keys.GET_STATUS
-//       })
-//     },
-//     async changeStatus(status) {
-//       await browser.runtime.sendMessage({
-//         type: keys.SET_STATUS,
-//         status
-//       })
-//       this.getStatus()
-//     },
-//     async removeNotification(notification) {
-//       this.notifications = this.notifications.filter(
-//         (item) => item.name !== notification.name
-//       )
-//       await browser.runtime.sendMessage({
-//         type: keys.REMOVE_NOTIFICATION,
-//         notification
-//       })
-//     },
-//     async triggerNotification(notification) {
-//       await browser.runtime.sendMessage({
-//         type: keys.TRIGGER_NOTIFICATION,
-//         notification
-//       })
-//     },
-//     async openListingPage(notification) {
-//       await browser.runtime.sendMessage({
-//         type: keys.OPEN_NOTIFICATION,
-//         notification
-//       })
-//     },
-//     itemCountsMessage(itemCount) {
-//       return this.translations.itemCounts.replace(
-//         '{{ITEMCOUNT}}',
-//         itemCount
-//       )
-//     }
+
 //   }
 // })
